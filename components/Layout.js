2e6a669e2dc5cde0e51c7305d7491330686bc6ca
thienjs <thienjsx@gmail.com>
2021-11-24 12:52:11 -0500

    Layout

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Head from "next/head"
+
+export default function Layout({title, children, keywords, description}) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='keywords' content={keywords}/>
+        <meta name='description' content={description}/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto my-7">{children}</main>
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+  title: "thien.me",
+  keywords: "web development, fullstack, coding, backend, frontend, react, nextjs, tailwindcss, strapi, css, javascript",
+  description: "welcome to my portfolio"
+}
